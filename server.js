48ebeb3b3e43e139bd44d544d74380b589d5796e
nurselaine <Elaine.t.huynh@gmail.com>
2022-08-08 19:29:02 -0700

    added logs for deployment

--- server.js
+++ server.js
@@ -8,12 +8,27 @@ const PORT = process.env.PORT || 3002;
 const fireDb = require('./modules/firestore');
 const message = require('./modules/twilio');
 
+
+console.log("Application Express Server Init:");
 app.use(express.json());
 app.use(cors());
+
+console.log("Application Express Server Config:");
 
 const sid = process.env.TWILIO_SID;
 const auth_token = process.env.TWILIO_AUTH_TOKEN
 const twilio = require('twilio')(sid, auth_token);
+
+console.log("Application Express Server Routes:");
+
+app.get('/', (req, res) => {
+  try {
+    console.log('Loann\'s salon page')
+    res.status(200).send('Hello');
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
 
 app.post('/reviews', fireDb.addReview);
 app.get('/reviews', fireDb.getAllReviews);
@@ -24,8 +39,12 @@ app.delete('/deleteReview/:id', fireDb.deleteReview);
 app.post('/appointments', message.appointment);
 app.post('/newReview', message.newReview);
 
+console.log("Application Express Server Route Error Handler:");
+
 app.get('*', res => {
   res.send('nothing is here. Try refreshing...')
 })
+
+console.log("Application Express Server Listen");
 
 app.listen(3001, () => console.log(`listening on localhost ${PORT}`));
