59b55e6647c6a28e8b9d02554b4cded27ff77ffb
nurselaine <Elaine.t.huynh@gmail.com>
2022-08-08 20:20:48 -0700

    removed parsing code for key

--- modules/firestore.js
+++ modules/firestore.js
@@ -11,11 +11,19 @@ console.log('firstore configuration');
 // console.log(process.env);
 
 const admin = require('firebase-admin');
+const creds = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+}
+
+console.log(creds);
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
