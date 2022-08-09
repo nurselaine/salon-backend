ca2713332903d510ec1ae6a16d1762b7e916e7d3
nurselaine <Elaine.t.huynh@gmail.com>
2022-08-08 20:26:04 -0700

    removed parsing code for key

--- modules/firestore.js
+++ modules/firestore.js
@@ -23,7 +23,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
