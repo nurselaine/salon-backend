dedf2e11db0daba11ae530fb0c4dc9c9de329f21
nurselaine <Elaine.t.huynh@gmail.com>
2022-08-08 19:59:42 -0700

    parsing firebase key

--- modules/firestore.js
+++ modules/firestore.js
@@ -8,14 +8,14 @@ app.use(cors());
 const message = require('./twilio');
 
 console.log('firstore configuration');
-console.log(process.env);
+// console.log(process.env);
 
 const admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
